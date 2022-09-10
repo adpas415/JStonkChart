b1070c58d0d9edf8adf9ce2f412d372f660ab767
adpas415 <anthonydanielpaschal@gmail.com>
2022-09-09 21:35:26 -0400

    Everything but the dataset and chart stacking.

--- src/info/monitorenter/gui/chart/demos/StonkChartDemo.java
+++ src/info/monitorenter/gui/chart/demos/StonkChartDemo.java
@@ -11,21 +11,58 @@ import info.monitorenter.gui.chart.labelformatters.formats.PriceFormat;
 import info.monitorenter.gui.chart.labelformatters.formats.TradingVolumeFormat;
 import info.monitorenter.gui.chart.pointpainters.PointPainterCandleStick;
 import info.monitorenter.gui.chart.pointpainters.PointPainterSimpleStick;
+import info.monitorenter.gui.chart.tracepoints.CandleStick;
+import info.monitorenter.gui.chart.tracepoints.SimpleStick;
 import info.monitorenter.gui.chart.traces.Trace2DPoints;
 import info.monitorenter.gui.chart.traces.Trace2DSimple;
 
 import java.awt.*;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class StonkChartDemo {
+
+    final Color
+        green  =  Color.decode("#28A49C"),
+        red    =  Color.decode("#F05851");
 
     final Chart2D
         price   =  new Chart2D(),
         volume  =  new Chart2D();
 
-    ITrace2D
+    final ITrace2D
         ohlc = new Trace2DPoints(new Trace2DSimple(), new PointPainterCandleStick()),
         vol  = new Trace2DPoints(new Trace2DSimple(), new PointPainterSimpleStick());
+
+    {
+        decorate(price);
+        decorate(volume);
+        yAxisPrice(price);
+        yAxisVolume(volume);
+        xAxisTime(volume);
+        setupToPaintCandles();
+    }
+
+    {
+
+        //todo: import an actual Intraday 1-min bar dataset, perhaps two or three days and we'll demonstrate skipping time blocks.
+        ZonedDateTime barTime = ZonedDateTime.of(LocalDate.of(1989, 7, 17), LocalTime.of(14, 30), ZoneId.of("America/New_York"));
+
+        paintCandle(barTime.toInstant().toEpochMilli(), 1, 2, 0.50, 1.50, 500);
+        barTime = barTime.plusMinutes(1);
+
+    }
+
+
+
+    public void paintCandle(long time, double open, double high, double low, double close, long volume) {
+        Color color = close > high ? green : red;
+        ohlc.addPoint(new CandleStick(time, open, high, low, close, color));
+        vol.addPoint(new SimpleStick(time, 0, volume, color));
+    }
 
     private static void decorate(Chart2D chart) {
         //chart.setFocusable(true);
@@ -76,10 +113,7 @@ public class StonkChartDemo {
 
     private void setupToPaintCandles() {
 
-        ohlc = new Trace2DPoints(new Trace2DSimple(), new PointPainterCandleStick());
         ohlc.setName("OHLC");
-
-        vol = new Trace2DPoints(new Trace2DSimple(), new PointPainterSimpleStick());
         vol.setName("VOLUME");
 
         price.addTrace(ohlc);
--- src/info/monitorenter/gui/chart/pointpainters/PointPainterSimpleStick.java
+++ src/info/monitorenter/gui/chart/pointpainters/PointPainterSimpleStick.java
@@ -1,29 +1,3 @@
-/*
- *  PointPainterCandleStick.java of project jchart2d, <enterpurposehere>. 
- *  Copyright (C) 2002 - 2012, Achim Westermann, created on Oct 9, 2012
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- * 
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- *  If you modify or optimize the code in a useful way please let me know.
- *  Achim.Westermann@gmx.de
- *
- *
- * File   : $Source: /cvsroot/jchart2d/jchart2d/codetemplates.xml,v $
- * Date   : $Date: 2009/02/24 16:45:41 $
- * Version: $Revision: 1.2 $
- */
-
 package info.monitorenter.gui.chart.pointpainters;
 
 import info.monitorenter.gui.chart.Chart2D;
@@ -32,7 +6,6 @@ import info.monitorenter.gui.chart.ITrace2D;
 import info.monitorenter.gui.chart.ITracePoint2D;
 import info.monitorenter.gui.chart.axis.AxisLinearSkipTimeBlocks;
 import info.monitorenter.gui.chart.axis.TimeBlock;
-import info.monitorenter.gui.chart.tracepoints.CandleStick;
 import info.monitorenter.gui.chart.tracepoints.SimpleStick;
 import info.monitorenter.gui.chart.traces.ATrace2D;
 
@@ -40,15 +13,6 @@ import java.awt.*;
 import java.util.LinkedList;
 import java.util.Set;
 
-/**
- * A special point painter that will only be useable to render instances of
- * {@link CandleStick}.
- * <p>
- * 
- * 
- * @author <a href="mailto:Achim.Westermann@gmx.de">Achim Westermann </a>
- * 
- */
 public class PointPainterSimpleStick extends APointPainter<PointPainterSimpleStick> {
 
   /** Generated <code>serialVersionUID</code>. **/
